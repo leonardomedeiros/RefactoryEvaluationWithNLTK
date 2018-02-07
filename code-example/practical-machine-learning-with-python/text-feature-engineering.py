4e19c9ec121e75b6efae6e73db767a2b38e0b93f
Unknown <leonardomelomedeiros@gmail.com>
2018-02-07 12:35:41 -0200

    aprendendo NLTK

---
+++ code-example/practical-machine-learning-with-python/text-feature-engineering.py
@@ -0,0 +1,65 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+import matplotlib.pyplot as plt
+
+pd.options.display.max_colwidth = 200
+
+
+corpus = ['The sky is blue and beautiful.',
+          'Love this blue and beautiful sky!',
+          'The quick brown fox jumps over the lazy dog.',
+          "A king's breakfast has sausages, ham, bacon, eggs, toast and beans",
+          'I love green eggs, ham, sausages and bacon!',
+          'The brown fox is quick and the blue dog is lazy!',
+          'The sky is very blue and the sky is very beautiful today',
+          'The dog is lazy but the brown fox is quick!'    
+]
+labels = ['weather', 'weather', 'animals', 'food', 'food', 'animals', 'weather', 'animals']
+
+corpus = np.array(corpus)
+print(corpus)
+
+corpus_df = pd.DataFrame({'Document': corpus, 
+                          'Category': labels})
+corpus_df = corpus_df[['Document', 'Category']]
+print(corpus_df)
+wpt = nltk.WordPunctTokenizer()
+stop_words = nltk.corpus.stopwords.words('english')
+
+def normalize_document(doc):
+    # lower case and remove special characters\whitespaces
+    doc = re.sub(r'[^a-zA-Z\s]', '', doc, re.I|re.A)
+    doc = doc.lower()
+    doc = doc.strip()
+    # tokenize document
+    tokens = wpt.tokenize(doc)
+    # filter stopwords out of document
+    filtered_tokens = [token for token in tokens if token not in stop_words]
+    # re-create document from filtered tokens
+    doc = ' '.join(filtered_tokens)
+    return doc
+
+normalize_corpus = np.vectorize(normalize_document)
+
+wpt = nltk.WordPunctTokenizer()
+stop_words = nltk.corpus.stopwords.words('english')
+
+def normalize_document(doc):
+    # lower case and remove special characters\whitespaces
+    doc = re.sub(r'[^a-zA-Z\s]', '', doc, re.I|re.A)
+    doc = doc.lower()
+    doc = doc.strip()
+    # tokenize document
+    tokens = wpt.tokenize(doc)
+    # filter stopwords out of document
+    filtered_tokens = [token for token in tokens if token not in stop_words]
+    # re-create document from filtered tokens
+    doc = ' '.join(filtered_tokens)
+    return doc
+
+normalize_corpus = np.vectorize(normalize_document)
+
+norm_corpus = normalize_corpus(corpus)
+print(norm_corpus)
